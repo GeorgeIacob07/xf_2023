aaf667a627b38033c8b74f1317d5a7a55ced555a
YoungBoyG69 <100873456+YoungBoyG69@users.noreply.github.com>
2023-11-19 14:15:28 +0200

    Add files via upload

---
+++ cars/cars/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xf_2023
+{
+    internal class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+
+        public Car(string nameName, string colortheme, int yearValue, int maxSpeedValue)
+        {
+            name = nameName;
+            color = colortheme;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of" + maxSpeed);
+
+        }
+    }
+}
---
+++ cars/cars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xf_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("Model", "red", 2022, 110);
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Ford.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.color);
+            Opel.fullThrottle();
+            Console.WriteLine(Volvo.name);
+            Console.WriteLine(Volvo.year);
+            Console.WriteLine(Volvo.color);
+            Volvo.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
